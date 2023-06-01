7e2caed71fcbb011429518ff239586a9a26d6135
BICHHANG\hang <21522041@gm.uit.edu.vn>
2023-06-01 21:07:56 +0700

    InputScoreScreen and EditScoreScreen

---
+++ EduConnectApp/EduConnectApp/ViewModel/EditScoreViewModel.cs
@@ -0,0 +1,25 @@
+﻿using EduConnectApp.Store;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduConnectApp.ViewModel
+{
+    public class EditScoreViewModel : BaseViewModel
+    {
+        public struct Subject
+        {
+            public string name {  get; set; }
+        }
+        private List<Subject> _SubjectList = new List<Subject>();
+        public List<Subject> SubjectList { get => _SubjectList; set { _SubjectList = value; OnPropertyChanged(); } }
+        public EditScoreViewModel(NavigationStore navigationStore)
+        {
+            SubjectList.Add(new Subject { name = "Toán"});
+            SubjectList.Add(new Subject { name = "Lý" });
+            SubjectList.Add(new Subject { name = "Hóa" });
+        }
+    }
+}
---
+++ EduConnectApp/EduConnectApp/ViewUCs/InputScore.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EduConnectApp.ViewUCs
+{
+    /// <summary>
+    /// Interaction logic for InputScore.xaml
+    /// </summary>
+    public partial class InputScore : UserControl
+    {
+        public InputScore()
+        {
+            InitializeComponent();
+        }
+    }
+}
