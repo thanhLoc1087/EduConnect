8dd41c08376792febe781f357063dbc051d0195c
21522611@gm.uit.edu.vn <21522611@gm.uit.edu.vn>
2023-06-12 01:21:08 +0700

    dev: Binding ClassView, ClassList

--- EduConnectApp/EduConnectApp/ViewModel/ClassListViewModel.cs
+++ EduConnectApp/EduConnectApp/ViewModel/ClassListViewModel.cs
@@ -10,6 +10,8 @@ using System.Windows.Controls.Primitives;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Xml.Serialization;
+using EduConnectApp.Model;
 
 namespace EduConnectApp.ViewModel
 {
@@ -28,31 +30,81 @@ namespace EduConnectApp.ViewModel
         public ICommand navClassListUC { get; }
         public ICommand navClassList { get; }
         public ICommand navMouse { get; }
+        public ICommand mouseEnter { get; }
+
+        private string _schoolYear;
+        public string schoolYear { get => _schoolYear; set { _schoolYear = value; OnPropertyChanged(); } }
+        private string _className;
+        public string className { get => _className; set { _className = value; OnPropertyChanged(); } }
+
+        private string _AmountSt;
+        public string AmountSt { get => _AmountSt; set { _AmountSt = value; OnPropertyChanged(); } }
+
+        private string _teacherName;
+        public string teacherName { get => _teacherName; set { _teacherName = value; OnPropertyChanged(); } }
 
 
         private List<Student> _StudentList = new List<Student>();
         public List<Student> StudentList { get => _StudentList; set { _StudentList = value; OnPropertyChanged(); } }
 
+        private ObservableCollection<HOCTAP> _Learning;
+        public ObservableCollection<HOCTAP> Learning { get => _Learning; set { _Learning = value; OnPropertyChanged(); } }
 
         public ClassListViewModel(NavigationStore navigationStore) {
+            ClassViewModel.AvailableClass classSelected = ClassViewModel.CurrentSelected;
 
+            //navigate
             navClassListUC = new NavigationCommand<HomeViewModel>(navigationStore, () => new HomeViewModel(navigationStore));
             navClassList = new NavigationCommand<StatisticViewModel>(navigationStore, () => new StatisticViewModel(navigationStore));
             //navMouse = new RelayCommand<DataGrid>((p) => { return true; }, (p) => _UpdateSpn(p));
+            mouseEnter = new RelayCommand<DataGrid>((p) => { return p.SelectedItem == null ? false : true; }, (p) => _MouseEnter(p));
 
+            Learning = new ObservableCollection<HOCTAP>(DataProvider.Ins.DB.HOCTAPs.Where(x => x.DELETED == false));
 
-            StudentList.Add(new Student { number = 1, Name = "Nguyễn Bình An", DOB = "5/10/2007", Gender = "Nữ", Phone = "415-954-1475", Address="TP HCM" });
-            StudentList.Add(new Student { number = 2, Name = "Đỗ Duy Dũng", DOB = "5/10/2007", Gender = "Nữ", Phone = "254-451-7893", Address="Đồng Nai" });
-            StudentList.Add(new Student { number = 3, Name = "Dennis Castillo", DOB = "5/10/2007", Gender = "Nữ", Phone = "125-520-0141", Address="TP HCM" });
-            StudentList.Add(new Student { number = 4, Name = "Gabriel Cox", DOB = "17/7/2007", Gender = "Nam", Phone = "808-635-1221", Address="TP HCM" });
-            StudentList.Add(new Student { number = 5, Name = "Lena Jones", DOB = "17/7/2007", Gender = "Nam", Phone = "320-658-9174" , Address = "Tiền Giang" });
-            StudentList.Add(new Student { number = 6, Name = "Benjamin Caliword", DOB = "17/7/2007", Gender = "Nữ", Phone = "114-203-6258", Address="TP HCM" });
-            StudentList.Add(new Student { number = 7, Name = "Sophia Muris", DOB = "19/5/2005", Gender = "Nữ", Phone = "852-233-6854", Address="TP HCM" });
-            StudentList.Add(new Student { number = 8, Name = "Ali Pormand", DOB = "19/5/2005", Gender = "Nữ", Phone = "968-378-4849", Address="TP HCM" });
-            StudentList.Add(new Student { number = 9, Name = "Frank Underwood", DOB = "19/5/2005", Gender = "Nữ", Phone = "301-584-6966" , Address = "TP HCM" });
-            StudentList.Add(new Student { number = 10, Name = "Saeed Dasman", DOB = "19/5/2005", Gender = "Nữ", Phone = "817-320-5052", Address="TP HCM" });
+            //student List
+            Student st = new Student();
+            st.number= 1;
+            DateTime dateTime;
+            foreach (HOCTAP ht in Learning )
+            {
+                if (ht.MALOP == classSelected.ClassID)
+                {
+                    var temp = DataProvider.Ins.DB.HOCSINHs.Where(x => x.MAHS == ht.MAHS && x.DELETED == false).FirstOrDefault();
+                    st.Name = temp.HOTEN;
+                    dateTime = (DateTime)temp.NTNS;
+                    st.DOB = dateTime.ToString("dd/MM/yyyy");
+                    st.Gender = temp.GIOITINH==true ? "Nữ" : "Nam";
+                    st.Phone = temp.SDT;
+                    st.Address=temp.DIACHI;
+                    StudentList.Add(st);
+                    st.number++;
+                }
+
+            }
+
+            //StudentList.Add(new Student { number = 1, Name = "Nguyễn Bình An", DOB = "5/10/2007", Gender = "Nữ", Phone = "415-954-1475", Address="TP HCM" });
+            //StudentList.Add(new Student { number = 2, Name = "Đỗ Duy Dũng", DOB = "5/10/2007", Gender = "Nữ", Phone = "254-451-7893", Address="Đồng Nai" });
+            //StudentList.Add(new Student { number = 3, Name = "Dennis Castillo", DOB = "5/10/2007", Gender = "Nữ", Phone = "125-520-0141", Address="TP HCM" });
+            //StudentList.Add(new Student { number = 4, Name = "Gabriel Cox", DOB = "17/7/2007", Gender = "Nam", Phone = "808-635-1221", Address="TP HCM" });
+            //StudentList.Add(new Student { number = 5, Name = "Lena Jones", DOB = "17/7/2007", Gender = "Nam", Phone = "320-658-9174" , Address = "Tiền Giang" });
+            //StudentList.Add(new Student { number = 6, Name = "Benjamin Caliword", DOB = "17/7/2007", Gender = "Nữ", Phone = "114-203-6258", Address="TP HCM" });
+            //StudentList.Add(new Student { number = 7, Name = "Sophia Muris", DOB = "19/5/2005", Gender = "Nữ", Phone = "852-233-6854", Address="TP HCM" });
+            //StudentList.Add(new Student { number = 8, Name = "Ali Pormand", DOB = "19/5/2005", Gender = "Nữ", Phone = "968-378-4849", Address="TP HCM" });
+            //StudentList.Add(new Student { number = 9, Name = "Frank Underwood", DOB = "19/5/2005", Gender = "Nữ", Phone = "301-584-6966" , Address = "TP HCM" });
+            //StudentList.Add(new Student { number = 10, Name = "Saeed Dasman", DOB = "19/5/2005", Gender = "Nữ", Phone = "817-320-5052", Address="TP HCM" });
+
+            //infoClass
+            schoolYear = Const.SchoolYear;
+            var temp2 = DataProvider.Ins.DB.LOPs.Where(x => x.MALOP == classSelected.ClassID && x.DELETED == false).FirstOrDefault();
+            var temp3 = DataProvider.Ins.DB.GIAOVIENs.Where(x => x.MAGV == temp2.GVCN && x.DELETED == false).FirstOrDefault();
+            teacherName = temp3.HOTEN;
+            className = temp2.TENLOP;
+            AmountSt = temp2.SISO.ToString() + " học sinh";
         }
 
-       
+       void _MouseEnter (DataGrid p)
+        {
+            p.IsEnabled= false;
+        }
     }
 }
--- EduConnectApp/EduConnectApp/ViewModel/ClassViewModel.cs
+++ EduConnectApp/EduConnectApp/ViewModel/ClassViewModel.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using EduConnectApp.Model;
 using System.Collections.ObjectModel;
 using EduConnectApp.UserControlCustom;
+using System.Windows;
 
 namespace EduConnectApp.ViewModel
 {
@@ -20,6 +21,7 @@ namespace EduConnectApp.ViewModel
     {
         public struct AvailableClass
         {
+            public int ClassID { get; set; }
             public string Grade { get; set; }
             public string Class { get; set; }
             public int NumofAttendants { get; set; }
@@ -43,56 +45,110 @@ namespace EduConnectApp.ViewModel
         private ObservableCollection<GIANGDAY> _Teaching;
         public ObservableCollection<GIANGDAY> Teaching { get => _Teaching; set { _Teaching = value; OnPropertyChanged(); } }
 
+        private string _schoolYear;
+        public string schoolYear { get => _schoolYear; set { _schoolYear = value; OnPropertyChanged(); } }
+        private string _email;
+        public string email { get => _email; set { _email = value; OnPropertyChanged(); } }
+        private string _address;
+        public string address { get => _address; set { _address = value; OnPropertyChanged(); } }
+        private string _school;
+        public string school { get => _school; set { _school = value; OnPropertyChanged(); } }
+        private string _group;
+        public string group { get => _group; set { _group = value; OnPropertyChanged(); } }
+        private string _subject1;
+        public string subject1 { get => _subject1; set { _subject1 = value; OnPropertyChanged(); } }
+        private string _subject2;
+        public string subject2 { get => _subject2; set { _subject2 = value; OnPropertyChanged(); } }
+        private string _subject3;
+        public string subject3 { get => _subject3; set { _subject3 = value; OnPropertyChanged(); } }
+        private string _vis1;
+        public string vis1 { get => _vis1; set { _vis1 = value; OnPropertyChanged(); } }
+        private string _vis2;
+        public string vis2 { get => _vis2; set { _vis2 = value; OnPropertyChanged(); } }
+        private string _vis3;
+        public string vis3 { get => _vis3; set { _vis3 = value; OnPropertyChanged(); } }
+
+        public static AvailableClass CurrentSelected { get; set; }
+
+
+
         public ClassViewModel(NavigationStore navigationStore)
         {
-            AvailableClass availableClass = new AvailableClass();
-            LoginWindow loginWindow = new LoginWindow();
-
-            var loginVM = loginWindow.DataContext as LoginViewModel;
-
-            Detail = new RelayCommand<DataGrid>((p) => { return true; }, (p) => _Detail(p));
-
-            navClassListUC = new NavigationCommand<ClassListViewModel>(navigationStore, () => new ClassListViewModel(navigationStore));
-
-            ClassList = new ObservableCollection<LOP>(DataProvider.Ins.DB.LOPs.Where(x => x.DELETED == false));
-            Teacher = new ObservableCollection<GIAOVIEN>(DataProvider.Ins.DB.GIAOVIENs.Where(x => x.DELETED == false));
-            Teaching = new ObservableCollection<GIANGDAY>(DataProvider.Ins.DB.GIANGDAYs.Where(x => x.DELETED == false));
-
-            //Lop chu nhiem
-            foreach (LOP lop in ClassList)
+            schoolYear = "NIÊN KHÓA " + Const.SchoolYear;
+            if (!Const.IsAdmin)
             {
-                if (lop.GVCN == loginVM.ID)
+
+                AvailableClass availableClass = new AvailableClass();
+                //LoginWindow loginWindow = new LoginWindow();
+
+                //var loginVM = loginWindow.DataContext as LoginViewModel;
+
+                Detail = new RelayCommand<DataGrid>((p) => { return p.SelectedItem == null ? false : true; }, (p) => _Detail(p));
+
+                //navigate
+                navClassListUC = new NavigationCommand<ClassListViewModel>(navigationStore, () => new ClassListViewModel(navigationStore));
+
+                //ListDTB
+                ClassList = new ObservableCollection<LOP>(DataProvider.Ins.DB.LOPs.Where(x => x.DELETED == false));
+                Teacher = new ObservableCollection<GIAOVIEN>(DataProvider.Ins.DB.GIAOVIENs.Where(x => x.DELETED == false));
+                Teaching = new ObservableCollection<GIANGDAY>(DataProvider.Ins.DB.GIANGDAYs.Where(x => x.DELETED == false));
+
+                //Infor
+                var temp = DataProvider.Ins.DB.GIAOVIENs.Where(x => x.MAGV == Const.KeyID && x.DELETED == false).FirstOrDefault();
+                email =  temp.EMAIL;
+                address = temp.DIACHI;
+                school = Const.School;
+                var temp2 = DataProvider.Ins.DB.TO1.Where(x => x.MATO == temp.MATO && x.DELETED == false).FirstOrDefault();
+                group = temp2.TENTO;
+
+                //Lop chu nhiem
+                foreach (LOP lop in ClassList)
                 {
-                    availableClass.Grade = lop.TENLOP.Substring(0, 1);
-                    availableClass.Class = lop.TENLOP;
-                    availableClass.NumofAttendants=(int)lop.SISO;
-                    foreach (GIAOVIEN gv in Teacher)
+                    if (lop.GVCN == Const.KeyID && lop.DELETED ==false)
                     {
-                        if (gv.MAGV == lop.GVCN) availableClass.Teacher = gv.HOTEN;
+                        availableClass.ClassID = lop.MALOP;
+                        availableClass.Grade = " " + lop.TENLOP.Substring(0, 2);
+                        availableClass.Class = lop.TENLOP;
+                        availableClass.NumofAttendants=(int)lop.SISO;
+                        foreach (GIAOVIEN gv in Teacher  )
+                        {
+                            if (gv.MAGV == lop.GVCN&& gv.DELETED ==false) availableClass.Teacher = gv.HOTEN;
+                        }
                     }
                 }
-            }
-            HomeroomList.Add(availableClass);
+                HomeroomList.Add(availableClass);
 
-            //Lop Giang day
+                //Lop Giang day
+                vis1 = "Collapsed";
+                vis2 = "Collapsed";
+                vis3 = "Collapsed";
+                int flag = 0;
+                ClassUC classuc = new ClassUC();
 
-            foreach (GIANGDAY gd in Teaching)
-            {
-                if (gd.MAGV == loginVM.ID)
+                foreach (GIANGDAY gd in Teaching)
                 {
-                    foreach (LOP lop in ClassList)
+                    if (gd.MAGV == Const.KeyID)
                     {
-                        if (lop.MALOP== gd.MALOP)
+                        foreach (LOP lop in ClassList)
                         {
-                            availableClass.Grade = lop.TENLOP.Substring(0, 1);
-                            availableClass.Class = lop.TENLOP;
-                            availableClass.NumofAttendants=(int)lop.SISO;
-                            foreach (GIAOVIEN gv in Teacher)
+                            if (lop.MALOP== gd.MALOP)
                             {
-                                if (gv.MAGV == lop.GVCN) availableClass.Teacher = gv.HOTEN;
+                                availableClass.ClassID = lop.MALOP;
+                                availableClass.Grade = lop.TENLOP.Substring(0, 2);
+                                availableClass.Class = lop.TENLOP;
+                                availableClass.NumofAttendants=(int)lop.SISO;
+                                foreach (GIAOVIEN gv in Teacher)
+                                {
+                                    if (gv.MAGV == lop.GVCN && gv.DELETED ==false) availableClass.Teacher = gv.HOTEN;
+                                }
+                                TeachingList.Add(availableClass);
                             }
-                            TeachingList.Add(availableClass);
                         }
+
+                        var temp3 = DataProvider.Ins.DB.MONHOCs.Where(x => x.MAMH == gd.MAMH && x.DELETED == false).FirstOrDefault();
+                        if (flag== 0) { subject1 = temp3.TENMH; flag=1; vis1 = "Visible";  }
+                        if (flag== 1 && subject1 != temp3.TENMH) { subject2= temp3.TENMH;flag =2; vis2 = "Visible"; }
+                        if (flag== 2 && subject1 != temp3.TENMH && subject2 != temp3.TENMH) { subject3= temp3.TENMH; vis1 = "Visible"; }
                     }
                 }
             }
@@ -100,6 +156,8 @@ namespace EduConnectApp.ViewModel
 
         void _Detail (DataGrid p )
         {
+            CurrentSelected =(AvailableClass)p.SelectedItem;
+
         }
     }
 }
