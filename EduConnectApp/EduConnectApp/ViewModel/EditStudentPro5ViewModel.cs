d9f05cd87a3b5e31f321d90faf16c6d8176b593a
21522611@gm.uit.edu.vn <21522611@gm.uit.edu.vn>
2023-06-12 18:12:49 +0700

    dev: fix UI + Binding + navigate screen ClassList

---
+++ EduConnectApp/EduConnectApp/ViewModel/EditStudentPro5ViewModel.cs
@@ -0,0 +1,21 @@
+﻿using EduConnectApp.Store;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduConnectApp.ViewModel
+{
+    public class EditStudentPro5ViewModel: BaseViewModel
+    {
+        private string _name;
+        public string name { get => _name; set { _name = value; OnPropertyChanged(); } }
+
+        public EditStudentPro5ViewModel(NavigationStore navigationStore)
+        {
+            ClassListViewModel.Student studentSelected = ClassListViewModel.CurrentSelected;
+            name = studentSelected.Name;
+        }
+    }
+}
--- EduConnectApp/EduConnectApp/ViewModel/StudentPro5ViewModel.cs
+++ EduConnectApp/EduConnectApp/ViewModel/StudentPro5ViewModel.cs
@@ -1,17 +1,31 @@
-﻿using EduConnectApp.Store;
+﻿using EduConnectApp.Commands;
+using EduConnectApp.Store;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace EduConnectApp.ViewModel
 {
     public class StudentPro5ViewModel: BaseViewModel
     {
+        private string _name;
+        public string name { get => _name; set { _name = value; OnPropertyChanged(); } }
+
+        public ICommand navBack { get; }
+        public ICommand navEdit { get; }
+
         public StudentPro5ViewModel(NavigationStore navigationStore)
         {
+            //navigate
+            navBack = new NavigationCommand<ClassListViewModel>(navigationStore, () => new ClassListViewModel(navigationStore));
+            navEdit = new NavigationCommand<EditStudentPro5ViewModel>(navigationStore, () => new EditStudentPro5ViewModel(navigationStore));
 
+            //value
+            ClassListViewModel.Student studentSelected = ClassListViewModel.CurrentSelected;
+            name = studentSelected.Name;
         }
     }
 }
