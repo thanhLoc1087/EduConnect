be95b6c7d99ccb725c9bac5b3210d84ee7b54e3b
TranChinh03 <21520166@gm.uit.edu.vn>
2023-06-19 15:38:05 +0700

    dev: export function

--- EduConnectApp/EduConnectApp/ViewUCs/ExportPro5.xaml.cs
+++ EduConnectApp/EduConnectApp/ViewUCs/ExportPro5.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,41 @@ namespace EduConnectApp.ViewUCs
         public ExportPro5()
         {
             InitializeComponent();
+        }
+
+        private void printBtn_Click(object sender, RoutedEventArgs e)
+        {
+            scroll.ScrollToVerticalOffset(0);
+            PrintDialog printDialog = new PrintDialog();
+            if (printDialog.ShowDialog() == true)
+            {
+                printDialog.PrintVisual(print, "Delivery Invoice");
+                MessageBox.Show("Xuất thành công!");
+            }
+        }
+
+        private void dtg_Scoretable_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            DataGrid dtg = sender as DataGrid;
+            ScrollViewer scv = FindParentScrollViewer(dtg);
+            scv.ScrollToVerticalOffset(scv.VerticalOffset - e.Delta);
+            e.Handled = true;
+        }
+        public static ScrollViewer FindParentScrollViewer(DependencyObject child)
+        {
+            // Check if the child object is null
+            if (child == null)
+                return null;
+
+            // Check if the current object is a ScrollViewer
+            if (child is ScrollViewer scrollViewer)
+                return scrollViewer;
+
+            // Get the parent of the child object
+            var parent = VisualTreeHelper.GetParent(child);
+
+            // Recursive call to find the ScrollViewer
+            return FindParentScrollViewer(parent);
         }
     }
 }
